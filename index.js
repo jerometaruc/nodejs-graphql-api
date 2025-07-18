3dfdb8b9197460ac6cec97f831864884fa05f9e4
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-18 15:12:23 -0700

    feat: add initial schema to Apollo Server

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// types
+import { typeDefs } from './schema.js';
+
 // Server setup
 const server = new ApolloServer({
-  // typeDefs
-  // resolvers
+    typeDefs,
+    // resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
+    listen: { port: 4000 }
 });
 
 console.log(`Server ready at: ${url}`);
